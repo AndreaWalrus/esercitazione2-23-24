3fdc6fbc4db2f43fd2424b1b9f86588b1ac4d602
AndreaWalrus <andrea@DESKTOP-I8F6GOQ>
2024-03-31 19:52:54 +0200

    Implemented pixel_bilinear and bilinear_resize

--- src/resize_image.cpp
+++ src/resize_image.cpp
@@ -18,16 +18,15 @@ float Image::pixel_nearest(float x, float y, int c) const
 // returns the bilinearly interpolated pixel (x,y,c)
 float Image::pixel_bilinear(float x, float y, int c) const
   {
-  // Since you are inside class Image you can
-  // use the member function pixel(a,b,c)
-  
-  
-  // TODO: Your code here
-  
-  NOT_IMPLEMENTED();
-  
-  
-  return 0;
+  int x_l = floor(x);
+  int y_l = floor(y);
+  int x_h = ceil(x);
+  int y_h = ceil(y);
+  float V1 = clamped_pixel(x_l,y_l,c);
+  float V2 = clamped_pixel(x_h,y_l,c);
+  float V3 = clamped_pixel(x_l,y_h,c);
+  float V4 = clamped_pixel(x_h,y_h,c);
+  return (V1*(x_h-x)*(y_h-y))+(V2*(x-x_l)*(y_h-y))+(V3*(x_h-x)*(y-y_l))+(V4*(x-x_l)*(y-y_l));
   }
 
 // HW1 #1
@@ -59,13 +58,20 @@ Image nearest_resize(const Image& im, int w, int h)
 // return new Image of size (w,h,im.c)
 Image bilinear_resize(const Image& im, int w, int h)
   {
-  
-  // TODO: Your code here
-  
-  NOT_IMPLEMENTED();
-  
-  
-  return Image();
+  Image ret(w,h,im.c);
+
+  for(int c=0; c<ret.c; c++){
+    for(int j=0; j<h; j++){
+      for(int i=0; i<w; i++){
+        float x_interpolated = -0.5+((i+0.5)/w)*im.w;
+        float y_interpolated = -0.5+((j+0.5)/h)*im.h;
+        float val = im.pixel_bilinear(x_interpolated,y_interpolated,c);
+        ret.set_pixel(i,j,c,val);
+      }
+    }
+  }
+
+  return ret;
   }
 
 
